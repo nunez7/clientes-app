004c346c8411ccf71af235a57468c0a60f9bc204
Felix Javier Nuñez Gonzalez <33491472+nunez7@users.noreply.github.com>
2022-09-21 11:09:14 -0600

    Implementando clienteService

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
-import { CLIENTES } from './clientes.json';
+import { ClienteService } from './cliente.service';
 
 @Component({
   selector: 'app-clientes',
@@ -10,11 +10,11 @@ export class ClientesComponent implements OnInit {
 
   clientes: Cliente[];
 
-  constructor() { }
+  constructor(private clienteService: ClienteService) { }
 
   //Asignamos los valores del array
   ngOnInit(): void {
-    this.clientes = CLIENTES;
+    this.clientes = this.clienteService.getClientes();
   }
 
 }
