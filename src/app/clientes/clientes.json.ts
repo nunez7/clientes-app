75c25da765bf584f29b30f4ea36097504a74399e
Felix Javier Nuñez Gonzalez <33491472+nunez7@users.noreply.github.com>
2022-09-21 10:53:29 -0600

    Clientes component

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,13 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+  {id: 1, nombre: 'Felix', apellido:'Nuñez', email:'felixjavier0@gmail.com', createAt: '2022-09-21'},
+  {id: 2, nombre: 'Juan', apellido:'Madrid', email:'juan23@gmail.com', createAt: '2022-09-21'},
+  {id: 3, nombre: 'Alberto', apellido:'Acosta', email:'albert33@gmail.com', createAt: '2022-09-21'},
+  {id: 4, nombre: 'Miriam', apellido:'Ledezma', email:'miriam33@gmail.com', createAt: '2022-09-21'},
+  {id: 5, nombre: 'María', apellido:'Duarte', email:'maria_a@gmail.com', createAt: '2022-09-21'},
+  {id: 6, nombre: 'Fernanda', apellido:'Cortez', email:'fernanda_cr@gmail.com', createAt: '2022-09-21'},
+  {id: 7, nombre: 'Patricia', apellido:'Martinez', email:'patricia_ma@gmail.com', createAt: '2022-09-21'},
+  {id: 8, nombre: 'Uriel', apellido:'Peña', email:'urielp@gmail.com', createAt: '2022-09-21'},
+  {id: 9, nombre: 'Julian', apellido:'Bañuelos', email:'julianb@gmail.com', createAt: '2022-09-21'},
+];
