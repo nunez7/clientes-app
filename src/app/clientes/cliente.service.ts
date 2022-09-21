ca752f02c7aee03558dd37909e5bbc3f382203d8
Felix Javier Nuñez Gonzalez <33491472+nunez7@users.noreply.github.com>
2022-09-21 15:23:14 -0600

    Implemantando service de spring

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,18 +1,23 @@
 import { Injectable } from '@angular/core';
-import { CLIENTES } from './clientes.json';
+//import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente';
-import { Observable, of } from 'rxjs';
+import { Observable, map } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
-
-  constructor() { }
+  private urlEndPoint:string = 'http://localhost:8883/backend-apirest/api/clientes';
+  //Establecemos la conexion
+  constructor(private http: HttpClient) { }
   //Observable es patron que verifica cambios
   //Of convierte los datos
   getClientes(): Observable<Cliente[]>{
-    return of(CLIENTES);
+    //return of(CLIENTES);
+    return this.http.get(this.urlEndPoint).pipe(
+      map((response) => response as Cliente[])
+    );
   }
 
 }
