9a341313d2be2e6819c9a204058aaec5e24cbf14
Felix Javier Nuñez Gonzalez <33491472+nunez7@users.noreply.github.com>
2022-09-22 10:27:44 -0600

    Implementando el borrar

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-clientes',
@@ -14,10 +15,38 @@ export class ClientesComponent implements OnInit {
 
   //Asignamos los valores del array
   ngOnInit(): void {
-   //Asigna el observador al array a traves de una funcion
-   this.clienteService.getClientes().subscribe(
+    //Asigna el observador al array a traves de una funcion
+    this.clienteService.getClientes().subscribe(
       (clientes) => this.clientes = clientes
-   );
+    );
+  }
+
+  delete(cliente: Cliente): void {
+    Swal.fire({
+      title: 'Estás seguro?',
+      text: "Borrarás el cliente",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, borrar!',
+      cancelButtonText: 'No, cancelar',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        //Borramos tras confirmar
+        this.clienteService.delete(cliente.id).subscribe(
+          response => {
+            //Actualizamos la lista
+            this.clientes = this.clientes.filter(cli => cli !== cliente)
+            Swal.fire(
+              'Cliente borrado!',
+              'Cliente eliminado con éxito',
+              'success'
+            )
+          }
+        )
+      }
+    })
   }
 
 }
