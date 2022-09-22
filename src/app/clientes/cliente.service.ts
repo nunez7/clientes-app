6a5ccdb57292d90e95fccace8da0ba7143534ddb
Felix Javier Nuñez Gonzalez <33491472+nunez7@users.noreply.github.com>
2022-09-22 08:42:32 -0600

    Creando nuevo cliente

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -2,13 +2,16 @@ import { Injectable } from '@angular/core';
 //import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente';
 import { Observable, map } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
   private urlEndPoint:string = 'http://localhost:8883/backend-apirest/api/clientes';
+
+  private HttpHeaders = new HttpHeaders({'Content-Type':'application/json'});
+
   //Establecemos la conexion
   constructor(private http: HttpClient) { }
   //Observable es patron que verifica cambios
@@ -18,6 +21,11 @@ export class ClienteService {
     return this.http.get(this.urlEndPoint).pipe(
       map((response) => response as Cliente[])
     );
+  }
+
+  //Implementando el guardar (create)
+  create(cliente: Cliente): Observable<Cliente>{
+    return this.http.post<Cliente>(this.urlEndPoint, cliente, {headers: this.HttpHeaders})
   }
 
 }
