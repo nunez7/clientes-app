dbb0cf6fa3ce992d825a144a32aa6a5a80c650e2
Felix Javier Nuñez Gonzalez <33491472+nunez7@users.noreply.github.com>
2022-10-04 12:57:30 -0600

    Select por default

--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -79,6 +79,9 @@ export class FormComponent implements OnInit {
   }
 
   compararRegion(o1: Region, o2: Region): boolean{
-    return o1 == null || o2 == null ? false: o1.id === o2.id;
+    if(o1 === undefined && o2==undefined){
+      return true;
+    }
+    return o1 === null || o2 === null || o1 === undefined || o2 === undefined ? false: o1.id === o2.id;
   }
 }
