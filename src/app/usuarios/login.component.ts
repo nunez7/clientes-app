6566a1d173e23cb73ade04d8423ed194a47a406d
Felix Javier Nuñez Gonzalez <33491472+nunez7@users.noreply.github.com>
2022-10-10 11:47:32 -0600

    Agregando el metodo login()
    Se agrego la clase Usuario
    Se agregaron los modelos en el formulario
    Se agrego el metodo login en el componente

--- src/app/usuarios/login.component.ts
+++ src/app/usuarios/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from './usuario';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
   titulo:string = 'Por favor Incia sesión';
+  usuario: Usuario;
 
-  constructor() { }
+  constructor() {
+    this.usuario = new Usuario();
+  }
 
   ngOnInit(): void {
+  }
+
+  login(): void{
+      console.log(this.usuario);
+      if(this.usuario.username==null || this.usuario.password==null){
+        Swal.fire('Error Login', 'Username o password vacíos', 'error');
+        return;
+      }
   }
 
 }
