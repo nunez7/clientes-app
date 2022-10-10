d57c77b317395582c87a42cbfba8fdcde86a9e90
Felix Javier Nuñez Gonzalez <33491472+nunez7@users.noreply.github.com>
2022-10-10 12:01:34 -0600

    Creando la clase AuthService

---
+++ src/app/usuarios/auth.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Usuario} from './usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  login(usuario: Usuario): Observable<any>{
+    //URL de peticion
+    const urlEndPoint = 'http://localhost:8883/backend-apirest/oauth/token';
+    //Credenciales de app
+    const credenciales = btoa('angularapp'+':'+'12345');
+    //Encabezados
+    const httpHeaders = new HttpHeaders({'Content-Type': 'application/x-www-form-urlencoded',
+    'Authorization': 'Basic '+credenciales});
+    //Parametros
+    let params = new URLSearchParams();
+    params.set('grant_type', 'password');
+    params.set('username', usuario.username);
+    params.set('password', usuario.password);
+    return this.http.post<any>(urlEndPoint, params, {headers: httpHeaders});
+  }
+}
